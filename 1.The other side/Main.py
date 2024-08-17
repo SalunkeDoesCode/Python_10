6260627b36c195693684a52b351ddd1cb9327e3b
SalunkeDoesCode <prasadsalunke5647@gmail.com>
2024-08-18 00:43:39 +0530

    New type geopoint added

--- 1.The other side/Main.py
+++ 1.The other side/Main.py
@@ -1,27 +1,31 @@
 from folium import Map
+from Geopoint import Geopoint
+
+latitude=float(input("Enter latitude:"))
+longitude=float(input("Enter longitude:"))
 
 
-
-latitude=str("40.09") #latitiude
-longitude=str("300.47") #longitude
-
-antipode_latitude=latitude.__mul__(int(("-1")))
+antipode_latitude=latitude * -1
 
 
-if longitude.__lt__(float("0")):
-    antipode_longitude=longitude.__add__(float("180"))
-elif longitude.__eq__(float("180")):
-    antipode_longitude=float("180")
-elif longitude.__gt__(float("180")):
-    antipode_longitude=str("Invalid Coordinates")    
+if longitude < 0.0:
+    antipode_longitude=longitude+180
+elif longitude==180:
+    antipode_longitude=180
+elif longitude>180:
+    antipode_longitude="Invalid Coordinates"
 else:
-    antipode_longitude=longitude.__sub__(float("180"))
+    antipode_longitude=longitude-180
 
-location=list((antipode_latitude, antipode_longitude)) 
+location=list((antipode_latitude, antipode_longitude))
 
-mymap = Map(location)    
+mymap = Map(location)
+mymap.save(str("antipode.html"))
 
+mypoint=Geopoint(antipode_latitude,antipode_longitude)
+cp=mypoint.closest_parallel()
 print(antipode_latitude)   
-print(antipode_longitude)                                   
-                                   
+print(antipode_longitude)         
+print("closest parallel is:",cp)
+# print(mymap)                  
  
